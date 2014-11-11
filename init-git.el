c26dd1d295962d806bd19b19dcec1b1dd64a6aea
Alexander Stepanenko <olexander314@gmail.com>
2014-11-12 01:42:46 +0300

    Fix git-messenger.

--- init-git.el
+++ init-git.el
@@ -5,6 +5,7 @@
 (global-set-key "\C-cgg" 'magit-status)
 
 (require-package 'git-messenger)
+(require 'git-messenger)
 (global-set-key (kbd "C-c g p") 'git-messenger:popup-message)
 (define-key git-messenger-map (kbd "m") 'git-messenger:copy-message)
 
