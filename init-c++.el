03602ecf5af1476abf21ec4bedc3f726487d3bd7
Alexander Stepanenko <olexander314@gmail.com>
2014-11-11 18:01:47 +0400

    Add line highlighting mode.

--- init-c++.el
+++ init-c++.el
@@ -29,6 +29,7 @@
 (add-hook 'c++-mode-hook 'indentation-c++-mode-hook)
 (add-hook 'c++-mode-hook 'subword-mode)
 (add-hook 'c++-mode-hook 'which-function-mode)
+(add-hook 'c++-mode-hook 'hl-line-mode)
 (require 'whitespace)
 (setq whitespace-line-column 80)
 (setq whitespace-style '(face lines-tail tabs))
