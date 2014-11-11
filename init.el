b000f4ee1ae21e328275b9ca7059fa3d744d69af
Alexander Stepanenko <olexander314@gmail.com>
2014-11-11 17:41:25 +0400

    Set the ibuffer as default.

--- init.el
+++ init.el
@@ -38,3 +38,5 @@
 (setq google-translate-default-source-language "en")
 (global-set-key "\C-ct" 'google-translate-at-point)
 (global-set-key "\C-cT" 'google-translate-query-translate)
+
+(defalias 'list-buffers 'ibuffer)
