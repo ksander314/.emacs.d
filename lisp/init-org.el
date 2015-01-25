5274e49012d0e30d2ba7580a4606587bccb95889
Alexander Stepanenko <olexander314@gmail.com>
2015-01-26 01:40:00 +0300

    Add org configurations.

---
+++ lisp/init-org.el
@@ -0,0 +1,9 @@
+(require-package 'gnuplot)
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((dot . t)
+   (gnuplot . t)))
+
+(provide 'init-org)
