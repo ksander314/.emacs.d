95a8a713ca3c51fc2cf685e365e38c8e1065a3a3
Alexander Stepanenko <astepanenko@tradingview.com>
2025-10-23 12:11:25 +0400

    feat(git): Add gptel-magit integration for improved Magit usage
    Implement gptel-magit setup and hook it into Magit mode. This
    enhances Git interactions by providing better context-aware
    functionalities while using Magit.

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -1,5 +1,6 @@
 (require-package 'git-gutter)
 (global-git-gutter-mode +1)
+(require-package 'gptel-magit)
 
 (require-package 'magit)
 (global-set-key "\C-cgg" 'magit-status)
@@ -8,5 +9,11 @@
 (require 'git-messenger)
 (global-set-key (kbd "C-c g p") 'git-messenger:popup-message)
 (define-key git-messenger-map (kbd "m") 'git-messenger:copy-message)
+
+(defun gptel-magit-setup ()
+  "Setup gptel-magit for Magit."
+  (gptel-magit-install))
+
+(add-hook 'magit-mode-hook 'gptel-magit-setup)
 
 (provide 'init-git)
