c0346694149c08f71c5d4777819546d298aeb981
Alexander Stepanenko <astepanenko@tradingview.com>
2025-10-25 18:43:25 +0400

    feat(rust): Enhance rust-mode setup with LSP and custom hooks
    Add support for LSP mode and improve rust-mode configuration by
    encapsulating settings in a dedicated setup function.  Enable
    keybindings for common Rust commands and streamline the setup of
    minor modes, ensuring better integration for Rust development.

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,45 +1,55 @@
 (require-package 'rust-mode)
 (require-package 'projectile)
-(require-package 'lsp-ui)
-(require 'rust-mode)
 (require-package 'lsp-mode)
+(require-package 'lsp-ui)
+(require-package 'cargo)
+(require-package 'toml-mode)
+(require-package 'flycheck-rust)
+
+(require 'rust-mode)
 (require 'lsp-mode)
-(autoload 'rust-mode "rust-mode" nil t)
-(defun my-rust-mode-hook ()
-  (local-set-key (kbd "C-c C-p") 'projectile-commander)
+(require 'lsp-ui)
+(require 'cargo)
+(require 'flycheck-rust)
+
+;; Define a function to encapsulate all rust-mode hooks and settings
+(defun my-rust-mode-setup ()
+  "Custom configurations and keybindings for rust-mode."
+  ;; Enable LSP mode and set configuration
+  (lsp)
   (setq lsp-rust-server 'rust-analyzer)
   (setq lsp-rust-analyzer-cargo-watch-command "clippy")
   (setq lsp-idle-delay 0.7)
   (setq lsp-rust-analyzer-server-display-inlay-hints t)
   
+  ;; Disable LSP UI documentation
   (setq lsp-ui-doc-enable nil)
-  (add-hook 'rust-mode-hook 'lsp-ui-mode)
-  )
-(add-hook 'rust-mode-hook 'my-rust-mode-hook)
-(add-hook 'rust-mode-hook
-          (lambda () (setq indent-tabs-mode nil)))
-(add-hook 'rust-mode-hook 'projectile-mode)
-(setq rust-format-on-save t)
-(define-key rust-mode-map (kbd "C-c c") 'rust-compile)
-(define-key rust-mode-map (kbd "C-c r") 'rust-run)
-(define-key rust-mode-map (kbd "C-c l") 'rust-run-clippy)
-(define-key rust-mode-map (kbd "C-c C-r") 'lsp-find-references)
-(define-key rust-mode-map (kbd "C-c C-i") 'lsp-find-implementation)
-(define-key rust-mode-map (kbd "C-c C-g") 'lsp-find-definition)
-(define-key rust-mode-map (kbd "C-c C-f") 'pop-tag-mark)
-(define-key rust-mode-map (kbd "C-c C-t") 'lsp-find-type-definition)
-(define-key rust-mode-map (kbd "C-c C-d") 'lsp-describe-thing-at-point)
-(define-key rust-mode-map (kbd "C-c t") 'cargo-process-current-test)
+  
+  ;; Enable necessary minor modes
+  (projectile-mode 1)
+  (cargo-minor-mode 1)
+  (hl-line-mode 1)
 
-(require-package 'toml-mode)
-(require 'toml-mode)
-(add-hook 'rust-mode-hook
-          (lambda () (lsp)))
-(require-package 'cargo)
-(require 'cargo)
-(add-hook 'rust-mode-hook 'cargo-minor-mode)
-(add-hook 'rust-mode-hook 'hl-line-mode)
+  ;; Enable formatted code on save
+  (setq rust-format-on-save t)
 
-(require-package 'flycheck-rust)
-(require 'flycheck-rust)
+  ;; Set indentation to spaces
+  (setq indent-tabs-mode nil)
+
+  ;; Local keybindings for Rust development
+  (define-key rust-mode-map (kbd "C-c C-p") 'projectile-commander)
+  (define-key rust-mode-map (kbd "C-c c") 'rust-compile)
+  (define-key rust-mode-map (kbd "C-c r") 'rust-run)
+  (define-key rust-mode-map (kbd "C-c l") 'rust-run-clippy)
+  (define-key rust-mode-map (kbd "C-c C-r") 'lsp-find-references)
+  (define-key rust-mode-map (kbd "C-c C-i") 'lsp-find-implementation)
+  (define-key rust-mode-map (kbd "C-c C-g") 'lsp-find-definition)
+  (define-key rust-mode-map (kbd "C-c C-f") 'pop-tag-mark)
+  (define-key rust-mode-map (kbd "C-c C-t") 'lsp-find-type-definition)
+  (define-key rust-mode-map (kbd "C-c C-d") 'lsp-describe-thing-at-point)
+  (define-key rust-mode-map (kbd "C-c t") 'cargo-process-current-test))
+
+;; Add hook to rust-mode
+(add-hook 'rust-mode-hook 'my-rust-mode-setup)
+
 (provide 'init-rust)
