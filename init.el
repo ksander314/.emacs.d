feda934b028b445eb5691bd68a47d65cbfce9a4b
Alexander Stepanenko <astepanenko@tradingview.com>
2025-11-10 18:47:03 +0400

    feat(init.el): Add functions to disable line numbers and whitespace
    Implement functionality to disable line numbers and trailing
    whitespace highlighting in specific modes. Add hooks for `term-mode`
    and `gptel-mode` to apply these settings.

--- init.el
+++ init.el
@@ -117,3 +117,16 @@
 
 (setq-default show-trailing-whitespace t)
 (column-number-mode t)
+
+(defun disable-line-numbers ()
+  "Disable line numbers in the current buffer."
+  (display-line-numbers-mode -1))
+
+(defun disable-trailing-whitespace ()
+  "Disable trailing whitespace highlighting in the current buffer."
+  (setq show-trailing-whitespace nil))
+
+(add-hook 'term-mode-hook 'disable-line-numbers)
+(add-hook 'gptel-mode-hook 'disable-line-numbers)
+(add-hook 'term-mode-hook 'disable-trailing-whitespace)
+(add-hook 'gptel-mode-hook 'disable-trailing-whitespace)
